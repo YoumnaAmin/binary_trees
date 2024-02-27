3a7dad82fbaf66d914c97f26a3215594346e62d5
YoumnaAmin <youmnamin564@gmail.com>
2024-02-28 01:33:31 +0300

    ..

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,44 +7,44 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-    if (newNode == NULL)
-        return (NULL);
+	if (newNode == NULL)
+		return (NULL);
 
-    newNode->n = value;
-    newNode->left = NULL;
-    newNode->right = NULL;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
-    if (parent == NULL)
-    {
+	if (parent == NULL)
+	{
 		parent = newNode;
-        return (newNode);
-    }
-    binary_tree_t *ptr = parent;
-    binary_tree_t *ptr2 = NULL;
+		return (newNode);
+	}
+	binary_tree_t *ptr = parent;
+	binary_tree_t *ptr2 = NULL;
 
-    while (ptr != NULL)
-    {
-        ptr2 = ptr;
-        if (value <= ptr->n)
-        {
-            ptr = ptr->left;
-        }
-        else
-        {
-            ptr = ptr->right;
-        }
-    }
+	while (ptr != NULL)
+	{
+		ptr2 = ptr;
+		if (value <= ptr->n)
+		{
+			ptr = ptr->left;
+		}
+		else
+		{
+			ptr = ptr->right;
+		}
+	}
 
-    if (value <= ptr2->n)
-    {
-        ptr2->left = newNode;
-    }
-    else
-    {
-        ptr2->right = newNode;
-    }
+	if (value <= ptr2->n)
+	{
+		ptr2->left = newNode;
+	}
+	else
+	{
+		ptr2->right = newNode;
+	}
 
-    return (newNode);
+	return (newNode);
 }
