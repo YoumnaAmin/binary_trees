b64bc0b918cb393a38a25420121c1d8a424b46c0
YoumnaAmin <youmnamin564@gmail.com>
2024-02-29 00:51:43 +0300

    ..

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,10 +1,12 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: the root node
  * @value: an int value
  * Return: a node type pointer
 */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
