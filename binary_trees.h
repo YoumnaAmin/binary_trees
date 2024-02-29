7045b49c1dfa0c792456385923f5fe61f6cb1adb
YoumnaAmin <youmnamin564@gmail.com>
2024-03-01 02:59:20 +0300

    ..

--- binary_trees.h
+++ binary_trees.h
@@ -42,6 +42,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+size_t binary_tree_height2(const binary_tree_t *tree);
+
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif
