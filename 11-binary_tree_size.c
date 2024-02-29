fd0e70279f2c6180e000de8a3d1891cd4281c55d
YoumnaAmin <youmnamin564@gmail.com>
2024-03-01 02:25:08 +0300

    ..

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,13 +7,13 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t sizel, sizer;
+	size_t sizel, sizer;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    sizel = binary_tree_size(tree->left);
-    sizer = binary_tree_size(tree->right);
+	sizel = binary_tree_size(tree->left);
+	sizer = binary_tree_size(tree->right);
 
-    return (1 + sizel + sizer);
+	return (1 + sizel + sizer);
 }
