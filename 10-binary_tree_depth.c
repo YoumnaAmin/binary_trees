9726849209c602d669d31251bb05a3644aa773e1
YoumnaAmin <youmnamin564@gmail.com>
2024-03-01 02:01:04 +0300

    ..

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,6 +13,6 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->parent)
-		depth = 1 + binary_tree_depth(tree->parent);
-	return (depth);
+		d = 1 + binary_tree_depth(tree->parent);
+	return (d);
 }
