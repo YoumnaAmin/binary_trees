e9b71aef138e4494e2531007d4a25a9b2de017e9
YoumnaAmin <youmnamin564@gmail.com>
2024-03-01 03:02:02 +0300

    ..

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,6 +29,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	balance = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+	balance = binary_tree_height2(tree->left) - binary_tree_height2(tree->right);
 	return (balance);
 }
