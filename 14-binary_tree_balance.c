bc267e3b0fe3323a545cf2308d9ed0d1c7bbced0
YoumnaAmin <youmnamin564@gmail.com>
2024-03-01 03:09:50 +0300

    ..

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,6 +6,7 @@
  *
  * Return: The height of the tree, or 0 if tree is NULL.
  */
+
 size_t binary_tree_height2(const binary_tree_t *tree)
 {
 	size_t height_l, height_r;
@@ -26,6 +27,8 @@ size_t binary_tree_height2(const binary_tree_t *tree)
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int balance;
+
 	if (!tree)
 		return (0);
 
